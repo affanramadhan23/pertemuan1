0e0f4b96155b54da54fb5fdd19bd952e2f540e7b
affanramadhan23 <affannabilramadhan@gmail.com>
2022-10-07 17:32:47 +0700

    public void BubbleSortArray

--- pertemuan1/Program.cs
+++ pertemuan1/Program.cs
@@ -50,5 +50,25 @@ namespace Bubble_Sort
             }
             Console.WriteLine("");
         }
+
+        public void BubbleSortArray()
+        {
+            for (int i = 0; i < n; i++) // For n - 1 passes
+            {
+                // Pada pass i, bandingkan n - i elemen pertama dengan elemen selanjutnya
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (a[j] < a[j + 1]) // jika elemen tidak dalam urutan yang benar
+                    {
+                        // Tukar elemen
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
     }
 }
